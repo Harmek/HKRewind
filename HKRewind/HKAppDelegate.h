d551e79823ff28975cff9f5ec459db499bb1bbe7
Panos Baroudjian <baroudjian.panos@gmail.com>
2013-07-01 01:54:20 +0200

    Initial Commit

---
+++ HKRewind/HKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HKAppDelegate.h
+//  HKRewind
+//
+//  Created by Panos Baroudjian on 7/1/13.
+//  Copyright (c) 2013 Panos Baroudjian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HKRewind/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HKRewind
+//
+//  Created by Panos Baroudjian on 7/1/13.
+//  Copyright (c) 2013 Panos Baroudjian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HKAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HKAppDelegate class]));
+    }
+}
